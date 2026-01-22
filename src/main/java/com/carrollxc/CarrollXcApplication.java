5996d3c191b07fb6d6d79d19107dd48fdc082c1c
Zack Gacnik <zgacnik@carroll.edu>
2026-01-22 16:44:32 -0700

    Initial commit

---
+++ src/main/java/com/carrollxc/CarrollXcApplication.java
@@ -0,0 +1,13 @@
+package com.carrollxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrollXcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrollXcApplication.class, args);
+    }
+
+}
